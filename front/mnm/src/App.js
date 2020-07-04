e0c979423350568125cf0079bcc4d83b0717683f
PCloud63514 <pcloud63514@gmail.com>
2020-07-04 19:00:38 +0900

    front-react branch

--- front/mnm/src/App.js
+++ front/mnm/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    isLoading: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (<div>
+      {isLoading ? "Loading" : "We are ready"}
+    </div>);
+  }
 }
 
-export default App;
+const data = [
+  {
+    id:1,
+    name: "커버낫",
+    image: "https://image.msscdn.net/images/goods_img/20180330/746280/746280_2_125.jpg",
+    rating: 5
+  },
+  {
+    id:2,
+    name: "내셔널지오그래픽",
+    image: "https://image.msscdn.net/images/goods_img/20190329/999287/999287_2_125.jpg",
+    rating: 4.3
+  },
+  {
+    id:3,
+    name: "로얄라이프",
+    image: "https://image.msscdn.net/images/goods_img/20170418/540421/540421_7_125.jpg",
+    rating: 2.7
+  },
+  {
+    id:4,
+    name: "로맨틱 파이어리츠",
+    image: "https://image.msscdn.net/images/goods_img/20190419/1021715/1021715_2_125.jpg",
+    rating: 3.6
+  },
+  {
+    id:5,
+    name: "프리즘웍스",
+    image: "https://image.msscdn.net/images/goods_img/20200513/1443918/1443918_1_125.jpg",
+    rating: 4.1    
+  },
+  {
+    id:6,
+    name: "트래블",
+    image: "https://image.msscdn.net/images/goods_img/20200502/1429495/1429495_1_125.jpg",
+    rating: 1.6
+  },
+];
---
+++ tutorial/GIt 오류 해결법.md
@@ -0,0 +1,17 @@
+# Git 오류 사항 정리 및 해결법
+
+- Repository not found
+
+  > 계정 로그인을 하지 않아서 발생하는 문제.
+
+  
+
+  해결법
+
+  <img src="./images/git_repository_not_found_1.PNG" width="50%" height="50%" title="git_repository_not_found_1" alt="git_repository_not_found_1"></img>
+
+  <img src="./images/git_repository_not_found_2.PNG" width="50%" height="50%" title="git_repository_not_found_2" alt="git_repository_not_found_2"></img>
+
+  <img src="./images/git_repository_not_found_3.PNG" width="50%" height="50%" title="git_repository_not_found_3" alt="git_repository_not_found_3"></img>
+
+  
